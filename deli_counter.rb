920b10538e2b702ee5ded0f8e1e56415bca740f9
antoninaorobets <pani.antonina.don@gmail.com>
2022-04-07 17:43:06 -0400

    commit tree b3f78601643bc3c545f4474a071d545d3f2ccc08

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+katz_deli = []
+
+def line katz_deli
+    line = ''
+    case katz_deli.length
+    when 0 then  puts "The line is currently empty."
+    else  katz_deli.each_with_index{|name,index| line << " #{index+1}. #{name}"}
+        puts  "The line is currently:" +line
+    end
+end
+
+def take_a_number (katz_deli, new_customer)
+    puts("Welcome, #{new_customer}. You are number #{katz_deli.length+1} in line.")
+    katz_deli << new_customer
+end
+
+def now_serving (katz_deli)
+    case katz_deli.length
+    when 0 then  puts "There is nobody waiting to be served!"
+    else  puts "Currently serving #{katz_deli[0]}." 
+    katz_deli.shift
+    end
+    katz_deli
+end
